49e7f82714abc5fea113619633dc2204924b9993
LIUISEI <a0966891512@gmail.com>
2023-07-15 19:23:22 +0900

    a

---
+++ Assets/Liu/StageManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    StageManager instance ;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    [SerializeField] int _time;
+    [SerializeField] int _stageScore;
+    
+
+
+
+
+
+
+
+}
