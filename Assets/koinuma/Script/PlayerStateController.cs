c2d1a5aacaeb4358544fbbcb41329714215f5780
showgo-koinuma <85728773+showgo-koinuma@users.noreply.github.com>
2023-07-19 18:19:32 +0900

    [add]ダメージを受けた処理追加

--- Assets/koinuma/Script/PlayerStateController.cs
+++ Assets/koinuma/Script/PlayerStateController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
+[RequireComponent (typeof(Camera))]
 public class PlayerStateController : MonoBehaviour
 {
     [SerializeField] Sprite _smallSprite;
     [SerializeField] Sprite _normalSprite;
     [SerializeField] Sprite _fireSprote;
+    Vector2 _smallSize = new Vector2(1, 1);
+    Vector2 _nomalSize = new Vector2(1, 2);
     State _playerState = State.Small;
     SpriteRenderer _sprite;
     CapsuleCollider2D _collider;
@@ -13,17 +16,19 @@ public class PlayerStateController : MonoBehaviour
     {
         _sprite = GetComponent<SpriteRenderer>();
         _collider = GetComponent<CapsuleCollider2D>();
+        _sprite.sprite = _smallSprite;
+        _collider.size = new Vector2(1, 1);
+
     }
 
     public void GetMashroom()
     {
-        Vector2 colliderSize = new Vector2(1, 2);
-        _collider.size = colliderSize;
         if (_playerState == State.Small)
         {
+            _playerState = State.Normal;
             _sprite.sprite = _normalSprite;
+            _collider.size = _nomalSize;
         }
-        _playerState = State.Normal;
     }
 
     public void GetFireFlower()
@@ -36,6 +41,26 @@ public class PlayerStateController : MonoBehaviour
             _sprite.color = new Color(1, 0.5f, 0); // スプライト設定したら消す
         }
         _playerState = State.Fire;
+    }
+
+    /// <summary>ダメージを受けたとき</summary>
+    public void GiveDamage()
+    {
+        if (_playerState == State.Small) // ちびのとき
+        {
+            // 死んだときのメソッドを呼び出す
+        }
+        else if (_playerState == State.Normal) // 通常のとき
+        {
+            _playerState = State.Small;
+            _sprite.sprite = _smallSprite;
+            _collider.size = _smallSize;
+        }
+        else // 能力状態のとき
+        {
+            _playerState = State.Normal;
+            _sprite.sprite = _normalSprite;
+        }
     }
 
     enum State
