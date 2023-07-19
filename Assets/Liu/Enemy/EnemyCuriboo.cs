72c14c22d2b794fb93d6f3303f06d114a0c20ce1
LIUISEI <a0966891512@gmail.com>
2023-07-19 19:00:28 +0900

    kuriboo

--- Assets/Liu/Enemy/EnemyCuriboo.cs
+++ Assets/Liu/Enemy/EnemyCuriboo.cs
@@ -4,48 +4,7 @@ using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class EnemyCuriboo : MonoBehaviour
+public class EnemyCuriboo : EnemyBase
 {
-    [SerializeField] float _moveSpeed = 2;
-    Rigidbody2D _rb;
-    int _dirX = 1;
-    bool _die;
-  
-
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        if (!_die)
-        {
-            _rb.velocity = new Vector2(_moveSpeed * _dirX, _rb.velocity.y);
-            Vector2 start = transform.position;
-            start += Vector2.right * _dirX * 0.509f;
-            Vector2 end = start + Vector2.right * _dirX * 0.01f;
-            Debug.DrawLine(start, end);
-            if (Physics2D.Linecast(start, end, 1))
-            {
-                _dirX *= -1;
-            }
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.TryGetComponent<PlayerController>(out PlayerController pc))
-        {
-            Destroy(gameObject,1);
-            _die = true;
-            pc.Jump();
-        }
-    }
-    public void Stanp()
-    {
-
-
-
-    }
+    
 }
