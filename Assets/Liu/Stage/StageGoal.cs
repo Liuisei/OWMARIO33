314ec3982f86d7a84980b67164086132f87015f0
LIUISEI <a0966891512@gmail.com>
2023-07-22 19:29:59 +0900

    3

---
+++ Assets/Liu/Stage/StageGoal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGoal : MonoBehaviour
+{
+    
+    Rigidbody2D _rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerController>(out PlayerController pc))
+        {
+            
+        }
+    }
+}
